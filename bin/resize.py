032cbf97d0d8bb57ee975d331d57146e5b6b4dc7
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2019-01-07 19:07:27 +0200

    resize exits fullscreen and removes max width and height on a window

--- bin/resize.py
+++ bin/resize.py
@@ -12,4 +12,4 @@ width, height = int(width), int(height)
 new_width = int(0.7 * width)
 start_pos_x = int((width - new_width) / 2)
 
-print(f"wmctrl -r :ACTIVE: -e 0,{start_pos_x},0,{new_width},{height}")
+print(f"wmctrl -r :ACTIVE: -b remove,fullscreen && wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz && wmctrl -r :ACTIVE: -e 0,{start_pos_x},0,{new_width},{height}")
