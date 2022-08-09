3bb066fe1c7e7f82a3fbb801f0194b9471dfe374
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2022-08-08 19:29:57 -0700

    feat: use floating window for nvim tree

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -1,6 +1,11 @@
 local nvim_tree = require "nvim-tree"
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
+local float_height = 30
+local float_row = math.floor((vim.api.nvim_win_get_height(0) - float_height) / 2)
+local float_width = 70
+local float_col = math.floor((vim.api.nvim_win_get_width(0) - float_width) / 2)
+
 nvim_tree.setup {
   sync_root_with_cwd = true,
   update_focused_file = {
@@ -8,6 +13,7 @@ nvim_tree.setup {
     update_root = true,
   },
   renderer = {
+    full_name = true,
     root_folder_modifier = ":t",
     icons = {
       glyphs = {
@@ -46,7 +52,7 @@ nvim_tree.setup {
     },
   },
   view = {
-    width = 50,
+    width = 70,
     height = 30,
     side = "left",
     relativenumber = true,
@@ -55,6 +61,17 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+      },
+    },
+    float = {
+      enable = true,
+      open_win_config = {
+        relative = "editor",
+        border = "rounded",
+        width = float_width,
+        height = float_height,
+        row = float_row,
+        col = float_col,
       },
     },
   },
