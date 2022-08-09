cd506fb5e05ae447e1596bfeb0a27825fc447c42
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2022-08-09 11:18:55 -0700

    feat(nvim): highlight yanked text

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -20,6 +20,18 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 
 local resize_nvim_tree = vim.api.nvim_create_augroup("ResizeNvimTree", { clear = true })
 vim.api.nvim_create_autocmd("VimResized", {
-  command = ":so " .. os.getenv('HOME') .. "/.dotfiles/nvim/lua/user/nvim-tree.lua",
+  command = ":so " .. os.getenv "HOME" .. "/.dotfiles/nvim/lua/user/nvim-tree.lua",
   group = resize_nvim_tree,
+})
+
+local yank_group = vim.api.nvim_create_augroup("HighlightYank", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = yank_group,
+  callback = function()
+    vim.highlight.on_yank {
+      higroup = "IncSearch",
+      timeout = 120,
+    }
+  end,
 })
