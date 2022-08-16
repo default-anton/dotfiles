a842be3953622fdb142da30ff3d526c154940252
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2022-08-16 15:52:03 -0700

    feat(nvim): search in directory

--- nvim/lua/user/fzf.lua
+++ nvim/lua/user/fzf.lua
@@ -1,4 +1,5 @@
 local fzf_lua = require "fzf-lua"
+local utils = require "fzf-lua.utils"
 
 fzf_lua.setup {
   winopts = {
@@ -25,7 +26,7 @@ fzf_lua.setup {
   },
 }
 
-local fzf_dirs = function(opts)
+local global_cd = function(opts)
   opts = opts or {}
   opts.prompt = "Directories> "
   opts.winopts = { width = 0.6, row = 0.6, height = 0.6 }
@@ -37,5 +38,46 @@ local fzf_dirs = function(opts)
   fzf_lua.fzf_exec("fd '' -t d -d 1 --hidden ~ ~/code .", opts)
 end
 
--- or to a keybind, both below are (sort of) equal
-vim.keymap.set("n", "<leader>cd", fzf_dirs)
+vim.keymap.set("n", "<leader>cd", global_cd)
+
+local select_cwd = function(callback)
+  local opts = {}
+  opts.prompt = "Directories> "
+  opts.winopts = { width = 0.6, row = 0.6, height = 0.6 }
+  opts.actions = {
+    ["default"] = function(selected)
+      callback(selected)
+    end,
+  }
+
+  fzf_lua.fzf_exec("fd -t d --no-ignore --hidden -E .git -E node_modules", opts)
+end
+
+vim.keymap.set("n", "<space>m", fzf_lua.live_grep_native)
+vim.keymap.set("n", "<space><space>m", function()
+  select_cwd(function(selected)
+    fzf_lua.live_grep_native { cwd = selected[1] }
+  end)
+end)
+vim.keymap.set("v", "<space>m", fzf_lua.grep_visual)
+vim.keymap.set("v", "<space><space>m", function()
+  local search = utils.get_visual_selection()
+
+  select_cwd(function(selected)
+    fzf_lua.grep { search = search, cwd = selected[1] }
+  end)
+end)
+vim.keymap.set("n", "<space>w", fzf_lua.grep_cword)
+vim.keymap.set("n", "<space><space>w", function()
+  select_cwd(function(selected)
+    fzf_lua.grep_cword { cwd = selected[1] }
+  end)
+end)
+vim.keymap.set("n", "<space><leader>", function()
+  fzf_lua.files { winopts = { row = 0.5, height = 0.5 } }
+end)
+vim.keymap.set("n", "<space><space><leader>", function()
+  select_cwd(function(selected)
+    fzf_lua.files { cwd = selected[1], winopts = { row = 0.5, height = 0.5 } }
+  end)
+end)
