113134fca0b14fe672d7335c64fd8153bcad4c63
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2022-08-26 22:08:54 -0700

    feat(nvim): install fidget to visualize lsp progress

--- nvim/lua/user/nvim-lsp.lua
+++ nvim/lua/user/nvim-lsp.lua
@@ -2,6 +2,8 @@ require("nvim-lsp-installer").setup {
   automatic_installation = true,
 }
 
+require("fidget").setup {}
+
 local lspconfig = require "lspconfig"
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
