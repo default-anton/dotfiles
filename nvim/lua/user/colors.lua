9fc74c5e90b1e17380803b089c2a66a47264dae8
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2022-09-21 15:47:47 -0700

    chore(nvim): upgrade tokyonight

--- nvim/lua/user/colors.lua
+++ nvim/lua/user/colors.lua
@@ -1,20 +1,16 @@
 local util = require "tokyonight.util"
 local colors = require("tokyonight.colors").setup()
 
-vim.g.tokyonight_lualine_bold = 1
+require("tokyonight").setup({
+  on_colors = function(colors)
+    colors.fg_gutter = colors.dark3
+    colors.gitSigns.add = "#1cb5af"
+    colors.gitSigns.change = "#386cd0"
+    colors.gitSigns.delete = "#ca3741"
+  end,
+})
+
 vim.opt.background = "dark"
 
-local git_signs = { add = "#164846", change = "#394b70", delete = "#823c41" }
+vim.cmd "colorscheme tokyonight-storm"
 
-vim.g.tokyonight_colors = {
-  fg_gutter = colors.dark3,
-  gitSigns = {
-    add = util.brighten(git_signs.add, 0.4),
-    change = util.brighten(git_signs.change, 0.4),
-    delete = util.brighten(git_signs.delete, 0.4),
-  },
-}
-
-vim.cmd "colorscheme tokyonight"
-
-util.highlight("CursorLineNr", { fg = util.darken(colors.yellow, 0.8) })
