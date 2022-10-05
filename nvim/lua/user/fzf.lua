c1b9a38a170311f71b4868a0a78120f87f5bd62a
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2022-10-05 08:57:02 -0700

    chore(nvim): delete unused search shortcuts

--- nvim/lua/user/fzf.lua
+++ nvim/lua/user/fzf.lua
@@ -41,45 +41,9 @@ local global_cd = function(opts)
 end
 
 vim.keymap.set("n", "<leader>cd", global_cd)
-
-local select_cwd = function(callback)
-  local opts = {}
-  opts.prompt = "Directories> "
-  opts.winopts = { width = 0.6, row = 0.6, height = 0.6 }
-  opts.actions = {
-    ["default"] = function(selected)
-      callback(selected)
-    end,
-  }
-
-  fzf_lua.fzf_exec("fd -t d --no-ignore --hidden -E .git -E node_modules", opts)
-end
-
 vim.keymap.set("n", "<space>m", fzf_lua.live_grep_native)
-vim.keymap.set("n", "<space><space>m", function()
-  select_cwd(function(selected)
-    fzf_lua.grep { search = "", cwd = selected[1] }
-  end)
-end)
 vim.keymap.set("v", "<space>m", fzf_lua.grep_visual)
-vim.keymap.set("v", "<space><space>m", function()
-  local search = utils.get_visual_selection()
-
-  select_cwd(function(selected)
-    fzf_lua.grep { search = search, cwd = selected[1] }
-  end)
-end)
 vim.keymap.set("n", "<space>w", fzf_lua.grep_cword)
-vim.keymap.set("n", "<space><space>w", function()
-  select_cwd(function(selected)
-    fzf_lua.grep_cword { cwd = selected[1] }
-  end)
-end)
 vim.keymap.set("n", "<space><leader>", function()
   fzf_lua.files { winopts = { row = 0.5, height = 0.5 } }
-end)
-vim.keymap.set("n", "<space><space><leader>", function()
-  select_cwd(function(selected)
-    fzf_lua.files { cwd = selected[1], winopts = { row = 0.5, height = 0.5 } }
-  end)
 end)
