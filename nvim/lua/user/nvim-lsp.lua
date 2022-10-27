a036c5116e3ae1591cc231bc656493cd1d6b627b
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2022-10-27 10:41:46 -0700

    chore(nvim): use cmp_nvim_lsp..default_capabilities()

--- nvim/lua/user/nvim-lsp.lua
+++ nvim/lua/user/nvim-lsp.lua
@@ -5,7 +5,7 @@ require("nvim-lsp-installer").setup {
 require("fidget").setup {}
 
 local lspconfig = require "lspconfig"
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Use an on_attach function to only map the following keys
