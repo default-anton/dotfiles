65780e14edf97e20440557d5f1606b3f74e38f3f
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-04-11 10:07:13 -0700

    fix(nvim): conflicting mappings

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -23,14 +23,8 @@ cmp.setup {
       c = cmp.mapping.close(),
     },
     ["<C-y>"] = cmp.mapping.confirm { select = true, behavior = cmp.ConfirmBehavior.Replace },
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<C-j>"] = cmp.mapping(function(fallback)
       cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
-    end, {
-      "i",
-      "s", --[[ "c" (to enable the mapping in command mode) ]]
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      cmp_ultisnips_mappings.jump_backwards(fallback)
     end, {
       "i",
       "s", --[[ "c" (to enable the mapping in command mode) ]]
