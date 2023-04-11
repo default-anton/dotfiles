3d91cf31c4380be3906e821e5b3ed72b806f6c86
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-04-11 09:38:31 -0700

    fix: nvim-tree mappings; use tab for copilot completions

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -8,40 +8,41 @@ local float_width = math.floor(math.min(90, win_width * 0.7))
 local float_row = math.floor((vim.api.nvim_win_get_height(0) - float_height) / 2)
 local float_col = math.floor((vim.api.nvim_win_get_width(0) - float_width) / 2)
 
+local M = {}
+
+function M.on_attach(bufnr)
+  local api = require('nvim-tree.api')
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', '<space>m', M.live_grep_native, { desc = 'Live grep', buffer = bufnr, noremap = true, silent = true, nowait = true })
+  vim.keymap.set('n', '<space>,', M.find_files, { desc = 'Find files', buffer = bufnr, noremap = true, silent = true, nowait = true })
+end
+
+function M.live_grep_native()
+  return M.launch_fzf("live_grep_native", {})
+end
+
+function M.find_files()
+  return M.launch_fzf("files", { winopts = { row = 0.5, height = 0.5 } })
+end
+
+function M.launch_fzf(func_name, opts)
+  local node = require("nvim-tree.lib").get_node_at_cursor()
+  local is_folder = node.fs_stat and node.fs_stat.type == "directory" or false
+  opts.cwd = is_folder and node.absolute_path or vim.fn.fnamemodify(node.absolute_path, ":h")
+  if node.name == ".." and TreeExplorer ~= nil then
+    opts.cwd = TreeExplorer.cwd
+  end
+
+  return require("fzf-lua")[func_name](opts)
+end
+
 nvim_tree.setup {
+  on_attach = M.on_attach,
   sync_root_with_cwd = true,
   update_focused_file = {
     enable = true,
     update_root = true,
-  },
-  renderer = {
-    full_name = true,
-    root_folder_modifier = ":t",
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "U",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
   },
   view = {
     adaptive_size = true,
@@ -71,35 +72,3 @@ nvim_tree.setup {
     ignore = false,
   },
 }
-
-local M = {}
-
-function M.live_grep_native()
-  return M.launch_fzf("live_grep_native", {})
-end
-
-function M.find_files()
-  return M.launch_fzf("files", { winopts = { row = 0.5, height = 0.5 } })
-end
-
-function M.launch_fzf(func_name, opts)
-  local nvim_tree_status_ok, _ = pcall(require, "nvim-tree")
-  if not nvim_tree_status_ok then
-    return
-  end
-  local lib_status_ok, lib = pcall(require, "nvim-tree.lib")
-  if not lib_status_ok then
-    return
-  end
-
-  local node = lib.get_node_at_cursor()
-  local is_folder = node.fs_stat and node.fs_stat.type == "directory" or false
-  opts.cwd = is_folder and node.absolute_path or vim.fn.fnamemodify(node.absolute_path, ":h")
-  if node.name == ".." and TreeExplorer ~= nil then
-    opts.cwd = TreeExplorer.cwd
-  end
-
-  return require("fzf-lua")[func_name](opts)
-end
-
-return M
--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -45,3 +45,7 @@ vim.g.markdown_fenced_languages = {
   "yaml",
   "dockerfile",
 }
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
