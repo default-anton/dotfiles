e6cf9cbd780cfa0cce56af4067d012c79dcae31b
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-06-08 09:09:36 -0700

    fix: gh pr view --web

--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -1,8 +1,8 @@
-vim.api.nvim_create_user_command("Pr", function()
+vim.api.nvim_create_user_command("pr", function()
   local Job = require "plenary.job"
 
   Job:new({
     command = "gh",
-    args = { "pr", "view", "-w" },
+    args = { "pr", "view", "--web" },
   }):start()
 end, {})
