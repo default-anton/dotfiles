623e71777beb14c9f8cd5ba1dcbbef69238478b1
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-06-28 15:27:56 -0700

    chore: remove autocmd for skhdrc and yabairc

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -1,18 +1,6 @@
 local restart_things = vim.api.nvim_create_augroup("RestartThings", { clear = true })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "skhdrc",
-  command = ":Dispatch! brew services restart skhd",
-  group = restart_things,
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "yabairc",
-  command = ":Dispatch! brew services restart yabai",
-  group = restart_things,
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "nvimrc, .nvimrc",
   command = ":so %",
   group = restart_things,
