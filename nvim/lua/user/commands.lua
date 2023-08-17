9309a1a4e28170acf0e24fc6c27f325b7f13aebe
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2023-08-17 11:35:55 -0700

    feat(nvim): Create pr if not exists

--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -1,8 +1,14 @@
 vim.api.nvim_create_user_command("Pr", function()
   local Job = require "plenary.job"
-
-  Job:new({
+  local open_pr = Job:new {
     command = "gh",
     args = { "pr", "view", "--web" },
-  }):start()
+  }
+
+  open_pr:add_on_exit_callback(vim.schedule_wrap(function(_, code)
+    if code ~= 0 then
+      vim.fn.system "gh pr create --web"
+    end
+  end))
+  open_pr:start()
 end, {})
