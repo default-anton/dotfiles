143834c88c8bab3c4c03e848e0306327f970d567
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-01-02 10:39:27 -0800

    chore(vim): Upgrade indent-blankline

--- nvim/lua/user/indent-blankline.lua
+++ nvim/lua/user/indent-blankline.lua
@@ -3,47 +3,9 @@ vim.g.indent_blankline_filetype_exclude = {
   "help",
   "NvimTree",
 }
-vim.g.indentLine_enabled = 1
--- vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_char = "▏"
--- vim.g.indent_blankline_char = "▎"
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-  "class",
-  "return",
-  "function",
-  "method",
-  "^if",
-  "^while",
-  "jsx_element",
-  "^for",
-  "^object",
-  "^table",
-  "block",
-  "arguments",
-  "if_statement",
-  "else_clause",
-  "jsx_element",
-  "jsx_self_closing_element",
-  "try_statement",
-  "catch_clause",
-  "import_statement",
-  "operation_type",
-}
--- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
 vim.wo.colorcolumn = "99999"
 
-require("indent_blankline").setup {
-  -- show_end_of_line = true,
-  -- space_char_blankline = " ",
-  show_current_context = true,
-  -- show_current_context_start = true,
-  -- char_highlight_list = {
-  --   "IndentBlanklineIndent1",
-  --   "IndentBlanklineIndent2",
-  --   "IndentBlanklineIndent3",
-  -- },
+require("ibl").setup {
+  scope = { enabled = true },
+  indent = { char = "▏" },
 }
