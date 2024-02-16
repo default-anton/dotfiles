b3dd1f6b8e067714d79e328fe42c673119c94785
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-02-16 11:16:36 -0800

    Fix go to definition in Rails

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,8 +1,21 @@
+local disabled_langs = { "ruby" }
+
 require("nvim-treesitter.configs").setup {
   -- A list of parser names, or "all"
   ensure_installed = { "html", "ruby", "lua", "bash", "python", "comment", "dockerfile", "gitattributes", "gitignore", "go", "gomod", "graphql", "hcl", "javascript", "jsdoc", "json", "make", "markdown", "proto", "scss", "sql", "toml", "tsx", "typescript", "vim", "yaml" },
   highlight = {
     enable = true,
+    disable = function(lang, buf)
+      if vim.tbl_contains(disabled_langs, lang) then
+        return true
+      end
+
+      local max_filesize = 100 * 1024   -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
   autopairs = {
     enable = true,
@@ -45,16 +58,16 @@ require("nvim-treesitter.configs").setup {
     disable = { "python", "css" },
   },
   matchup = {
-    enable = true,              -- mandatory, false will disable the whole extension
+    enable = true, -- mandatory, false will disable the whole extension
     disable = {},  -- optional, list of language that will be disabled
   },
 }
 
 require("treesitter-context").setup {
-  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  enable = true,        -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0,        -- How many lines the window should span. Values <= 0 mean no limit.
   trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+  patterns = {          -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
     -- For all filetypes
     -- Note that setting an entry here replaces all other patterns for this entry.
     -- By setting the 'default' entry below, you can control which nodes you want to
@@ -85,6 +98,6 @@ require("treesitter-context").setup {
   -- [!] The options below are exposed but shouldn't require your attention,
   --     you can safely ignore them.
 
-  zindex = 20, -- The Z-index of the context window
+  zindex = 20,     -- The Z-index of the context window
   mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
 }
