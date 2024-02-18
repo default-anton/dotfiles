e6b18de4ae15aad282c2e61ffa0c98e09b45fb34
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-02-17 22:27:04 -0800

    feat: highlight copied text 30ms longer

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -13,7 +13,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank {
       higroup = "IncSearch",
-      timeout = 120,
+      timeout = 150,
     }
   end,
 })
