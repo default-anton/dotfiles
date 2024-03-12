2dc6c8695efdc4b01d1ed1e5e41257b74d138b09
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-03-12 11:50:41 -0700

    feat: create :Replace command to replace a string in all files

--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -12,3 +12,27 @@ vim.api.nvim_create_user_command("Pr", function()
   end))
   open_pr:start()
 end, {})
+
+-- Replace a string in all files in the current directory
+vim.api.nvim_create_user_command("Replace", function()
+  local replace = vim.fn.input "Replace: "
+  if vim.trim(replace) == "" then
+    return
+  end
+
+  local with = vim.fn.input "With: "
+  if vim.trim(with) == "" then
+    return
+  end
+
+  local rg_command = string.format("rg -w '%s' -l", replace)
+  local rg_output = vim.fn.systemlist(rg_command)
+
+  vim.cmd("args " .. table.concat(rg_output, " "))
+
+  vim.cmd(string.format(
+    "argdo %%s/%s/%s/ge | update",
+    replace,
+    with
+  ))
+end, { nargs = 0 })
