d500f74cbf5aae0537f01e186c0f4b34180b73cf
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-03-13 10:44:17 -0700

    chore: formatting

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -10,7 +10,7 @@ require("nvim-treesitter.configs").setup {
         return true
       end
 
-      local max_filesize = 100 * 1024   -- 100 KB
+      local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
         return true
