4da7c9eb2ab5534ca325f1776ac292432926b8e1
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-03-14 09:58:32 -0700

    imp: center the screen while scrolling through the quickfix list

--- nvim/lua/user/maps.lua
+++ nvim/lua/user/maps.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_set_keymap('n', '<leader>qo', ':copen<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>qc', ':cclose<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qn', ':cn<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>qp', ':cp<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>qn', ':cn<CR>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>qp', ':cp<CR>zz', { noremap = true, silent = true })
