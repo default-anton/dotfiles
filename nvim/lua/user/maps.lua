0e1755c3936909e7e9772c9b30b71308a66062d8
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-04-16 09:53:26 -0700

    chore: translate maps from vimscript to lua

--- nvim/lua/user/maps.lua
+++ nvim/lua/user/maps.lua
@@ -7,3 +7,29 @@ vim.api.nvim_set_keymap('n', '<leader>ao', '<cmd>Ask<CR>', { noremap = true, sil
 vim.api.nvim_set_keymap('v', '<leader>ao', ':\'<,\'>Ask<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ac', '<cmd>Ask claude-3-opus-20240229<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>ac', ':\'<,\'>Ask claude-3-opus-20240229<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>rf', '<cmd>TestFile<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>rr', '<cmd>TestNearest<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>rl', '<cmd>TestLast<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ra', '<cmd>TestSuite<CR>', { noremap = true, silent = true })
+
+
+-- LazyGit integration
+vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>LazyGit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gf', '<cmd>LazyGitFilterCurrentFile<CR>', { noremap = true, silent = true })
+
+-- Resize splits with Ctrl-Shift-Arrow keys
+vim.api.nvim_set_keymap('n', '<C-S-Up>', '<cmd>resize -6<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-S-Down>', '<cmd>resize +6<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-S-Left>', '<cmd>vertical resize -6<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-S-Right>', '<cmd>vertical resize +6<CR>', { noremap = true, silent = true })
+
+-- Window navigation using Ctrl + hjkl
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-W>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-W>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-W>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-W>l', { noremap = true, silent = true })
+
+-- Enhanced scrolling with Ctrl-d/u
+vim.api.nvim_set_keymap('n', '<C-d>', '8jzzzv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '8kzzzv', { noremap = true, silent = true })
