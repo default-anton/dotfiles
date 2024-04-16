1f421c4f136a0b206a5235c109f4f85ebd6c54a3
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-04-16 10:11:42 -0700

    feat: add yank to clipboard

--- nvim/lua/user/maps.lua
+++ nvim/lua/user/maps.lua
@@ -1,18 +1,20 @@
+-- Quickfix list navigation
 vim.api.nvim_set_keymap('n', '<leader>qo', ':copen<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>qc', ':cclose<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>qn', ':cn<CR>zz', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>qp', ':cp<CR>zz', { noremap = true, silent = true })
 
+-- LLM integration
 vim.api.nvim_set_keymap('n', '<leader>ao', '<cmd>Ask<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>ao', ':\'<,\'>Ask<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ac', '<cmd>Ask claude-3-opus-20240229<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>ac', ':\'<,\'>Ask claude-3-opus-20240229<CR>', { noremap = true, silent = true })
 
+-- Run tests
 vim.api.nvim_set_keymap('n', '<leader>rf', '<cmd>TestFile<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>rr', '<cmd>TestNearest<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>rl', '<cmd>TestLast<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ra', '<cmd>TestSuite<CR>', { noremap = true, silent = true })
-
 
 -- LazyGit integration
 vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>LazyGit<CR>', { noremap = true, silent = true })
@@ -33,3 +35,6 @@ vim.api.nvim_set_keymap('n', '<C-l>', '<C-W>l', { noremap = true, silent = true 
 -- Enhanced scrolling with Ctrl-d/u
 vim.api.nvim_set_keymap('n', '<C-d>', '8jzzzv', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-u>', '8kzzzv', { noremap = true, silent = true })
+
+-- Yank to clipboard
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
