d1382d5e63f9e74b1bd5a7e353fb823441295d92
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-04-29 12:21:45 -0700

    feat: Add gpt-4 key bindings

--- nvim/lua/user/maps.lua
+++ nvim/lua/user/maps.lua
@@ -5,8 +5,10 @@ vim.api.nvim_set_keymap('n', '<leader>qn', ':cn<CR>zz', { noremap = true, silent
 vim.api.nvim_set_keymap('n', '<leader>qp', ':cp<CR>zz', { noremap = true, silent = true })
 
 -- LLM integration
-vim.api.nvim_set_keymap('n', '<leader>ao', '<cmd>Ask<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>ao', ':\'<,\'>Ask<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>aa', '<cmd>Ask<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>aa', ':\'<,\'>Ask<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ao', '<cmd>Ask gpt-4-turbo-2024-04-09<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>ao', ':\'<,\'>Ask gpt-4-turbo-2024-04-09<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ac', '<cmd>Ask claude-3-opus-20240229<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>ac', ':\'<,\'>Ask claude-3-opus-20240229<CR>', { noremap = true, silent = true })
 
