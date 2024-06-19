24289f034a64237cdd78a072d54cd93b944e6780
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-06-19 14:27:12 -0700

    chore: delete defaults

--- nvim/lua/user/gitsigns.lua
+++ nvim/lua/user/gitsigns.lua
@@ -1,87 +1,40 @@
 local gs = require "gitsigns"
 
-local default_opts = { noremap = true, silent = true }
-
 gs.setup {
   on_attach = function(bufnr)
     local function map(mode, l, r, opts)
-      opts = vim.tbl_extend("force", default_opts, opts or {})
+      opts = opts or {}
       opts.buffer = bufnr
       vim.keymap.set(mode, l, r, opts)
     end
 
     -- Navigation
-    map("n", "]c", function()
+    map('n', ']c', function()
       if vim.wo.diff then
-        return "]c"
+        vim.cmd.normal({ ']c', bang = true })
+      else
+        gs.nav_hunk('next')
       end
-      vim.schedule(function()
-        gs.next_hunk()
-      end)
-      return "<Ignore>"
-    end, { expr = true })
+    end)
 
-    map("n", "[c", function()
+    map('n', '[c', function()
       if vim.wo.diff then
-        return "[c"
+        vim.cmd.normal({ '[c', bang = true })
+      else
+        gs.nav_hunk('prev')
       end
-      vim.schedule(function()
-        gs.prev_hunk()
-      end)
-      return "<Ignore>"
-    end, { expr = true })
+    end)
 
     -- Actions
     map("n", "<leader>ht", gs.toggle_signs)
-    map({ "n", "v" }, "<leader>hs", gs.stage_hunk)
-    map({ "n", "v" }, "<leader>hr", gs.reset_hunk)
+    map("n", "<leader>hs", gs.stage_hunk)
+    map("n", "<leader>hr", gs.reset_hunk)
+    map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+    map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
     map("n", "<leader>hS", gs.stage_buffer)
     map("n", "<leader>hR", gs.reset_buffer)
     map("n", "<leader>hu", gs.undo_stage_hunk)
     map("n", "<leader>hp", gs.preview_hunk)
-    map("n", "<leader>hb", function()
-      gs.blame_line { full = true }
-    end)
-  end,
-  signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-  },
-  signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-  watch_gitdir = {
-    interval = 1000,
-    follow_files = true,
-  },
-  attach_to_untracked = true,
-  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
-    ignore_whitespace = false,
-  },
-  current_line_blame_formatter_opts = {
-    relative_time = false,
-  },
-  sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil, -- Use default
-  max_file_length = 40000,
-  preview_config = {
-    -- Options passed to nvim_open_win
-    border = "single",
-    style = "minimal",
-    relative = "cursor",
-    row = 0,
-    col = 1,
-  },
-  yadm = {
-    enable = false,
-  },
+    map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+  end
 }
