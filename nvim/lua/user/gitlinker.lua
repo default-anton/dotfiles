e577f25ac0d6e1de5cf5a2b62d60ef52014810f8
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-08-08 14:55:33 -0700

    feat: add desc to gitlinker maps

--- nvim/lua/user/gitlinker.lua
+++ nvim/lua/user/gitlinker.lua
@@ -6,23 +6,26 @@ vim.api.nvim_set_keymap(
   "n",
   "<leader>cr",
   '<cmd>lua require"gitlinker".get_buf_range_url("n")<cr>',
-  { silent = true }
+  { silent = true, desc = "Copy git link for current line" }
 )
+
 vim.api.nvim_set_keymap(
   "v",
   "<leader>cr",
   '<cmd>lua require"gitlinker".get_buf_range_url("v")<cr>',
-  {}
+  { desc = "Copy git link for selected lines" }
 )
+
 vim.api.nvim_set_keymap(
   "n",
   "<leader>gr",
   '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr><cr>',
-  { silent = true }
+  { silent = true, desc = "Open git link for current line in browser" }
 )
+
 vim.api.nvim_set_keymap(
   "v",
   "<leader>gr",
   '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr><cr>',
-  {}
+  { desc = "Open git link for selected lines in browser" }
 )
