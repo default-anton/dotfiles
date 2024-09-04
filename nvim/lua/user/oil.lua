b74919ad98258937cde5462b65fffe2cc306b222
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-09-04 12:12:29 -0700

    feat: <leader>ad to add folders to llm context

--- nvim/lua/user/oil.lua
+++ nvim/lua/user/oil.lua
@@ -1,17 +1,78 @@
 local telescope = require "telescope.builtin"
 local oil = require("oil")
 
+local get_dir_under_cursor = function()
+  local entry = oil.get_cursor_entry()
+  local cwd = oil.get_current_dir()
+
+  if entry and cwd then
+    if entry.type == "directory" then
+      return cwd .. entry.parsed_name
+    else
+      return cwd
+    end
+  end
+
+  return nil
+end
+
 local live_grep_in_dir = {
   desc = "Live grep in directory",
-  callback = function(opts)
-    telescope.live_grep { search_dirs = { oil.get_current_dir() } }
+  callback = function(_)
+    local search_dirs = {}
+    local dir = get_dir_under_cursor()
+    if dir then
+      search_dirs = { dir }
+    end
+    telescope.live_grep { search_dirs = search_dirs }
   end,
 }
 
 local find_files_in_dir = {
   desc = "Find files in directory",
-  callback = function(opts)
-    telescope.find_files { search_dirs = { oil.get_current_dir() } }
+  callback = function(_)
+    local search_dirs = {}
+    local dir = get_dir_under_cursor()
+    if dir then
+      search_dirs = { dir }
+    end
+    telescope.find_files { search_dirs = search_dirs }
+  end,
+}
+
+local add_to_llm_context = {
+  desc = "Add to LLM context",
+  callback = function(_)
+    local entry = oil.get_cursor_entry()
+    local cwd = oil.get_current_dir()
+    if entry and cwd then
+      local path
+      if entry.type == "directory" then
+        path = cwd .. entry.parsed_name
+      else
+        vim.cmd('Add ' .. cwd)
+        return
+      end
+      
+      local function add_files_recursively(dir)
+        local handle = vim.loop.fs_scandir(dir)
+        if not handle then return end
+
+        while true do
+          local name, type = vim.loop.fs_scandir_next(handle)
+          if not name then break end
+
+          local full_path = dir .. '/' .. name
+          if type == 'file' then
+            vim.cmd('Add ' .. full_path)
+          elseif type == 'directory' then
+            add_files_recursively(full_path)
+          end
+        end
+      end
+
+      add_files_recursively(path)
+    end
   end,
 }
 
@@ -32,6 +93,7 @@ oil.setup({
   keymaps = {
     ["<leader>m"] = live_grep_in_dir,
     ["<leader>,"] = find_files_in_dir,
+    ["<leader>ad"] = add_to_llm_context,
   },
 })
 
