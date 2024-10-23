ad460df8851400867d51a64614ad593223dbd0ae
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-10-23 09:42:01 -0700

    feat: call :Cmd of no llm buf isn't open

--- nvim/lua/user/oil.lua
+++ nvim/lua/user/oil.lua
@@ -40,38 +40,51 @@ local find_files_in_dir = {
   end,
 }
 
+local function add_files_under_cursor_to_llm_context(entry, cwd)
+  if entry and cwd then
+    if entry.type ~= "directory" then
+      local full_path = vim.fn.fnameescape(cwd .. entry.parsed_name)
+      vim.cmd('Add ' .. full_path)
+      return
+    end
+    local path = cwd .. entry.parsed_name
+
+    local function add_files_recursively(dir)
+      local handle = vim.loop.fs_scandir(dir)
+      if not handle then return end
+
+      while true do
+        local name, type = vim.loop.fs_scandir_next(handle)
+        if not name then break end
+
+        local full_path = vim.fn.fnameescape(dir .. '/' .. name)
+        if type == 'file' then
+          vim.cmd('Add ' .. full_path)
+        elseif type == 'directory' then
+          add_files_recursively(dir .. '/' .. name)
+        end
+      end
+    end
+
+    add_files_recursively(path)
+  end
+end
+
 local add_to_llm_context = {
   desc = "Add to LLM context",
   callback = function(_)
     local entry = oil.get_cursor_entry()
     local cwd = oil.get_current_dir()
-    if entry and cwd then
-      local path
-      if entry.type == "directory" then
-        path = cwd .. entry.parsed_name
-      else
-        vim.cmd('Add ' .. cwd .. entry.parsed_name)
-        return
-      end
-      local function add_files_recursively(dir)
-        local handle = vim.loop.fs_scandir(dir)
-        if not handle then return end
-
-        while true do
-          local name, type = vim.loop.fs_scandir_next(handle)
-          if not name then break end
-
-          local full_path = dir .. '/' .. name
-          if type == 'file' then
-            vim.cmd('Add ' .. full_path)
-          elseif type == 'directory' then
-            add_files_recursively(full_path)
-          end
-        end
-      end
-
-      add_files_recursively(path)
+    
+    if not vim.g.llm_sidekick_last_ask_buffer or not vim.api.nvim_buf_is_valid(vim.g.llm_sidekick_last_ask_buffer) then
+      vim.cmd('Code')
+      -- Schedule to ensure the buffer is ready
+      vim.schedule(function()
+        add_files_under_cursor_to_llm_context(entry, cwd)
+      end)
+      return
     end
+    add_files_under_cursor_to_llm_context(entry, cwd)
   end,
 }
 
@@ -83,7 +96,7 @@ oil.setup({
   skip_confirm_for_simple_edits = true,
   view_options = {
     show_hidden = true,
-    is_always_hidden = function(name, bufnr)
+    is_always_hidden = function(name, _)
       if name == ".." or name == ".git" then
         return true
       end
