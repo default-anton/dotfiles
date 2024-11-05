956f50640c2bd4a529a1e21933631b89c76ba6b6
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-11-04 21:36:35 -0800

    feat: Strip cmd to strip whitespace

--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -20,3 +20,25 @@ vim.api.nvim_create_user_command("Ctest", function()
   vim.fn.mkdir(spec_dir, "p")
   vim.cmd("edit " .. spec_file)
 end, { desc = "Create a spec file for the current file" })
+
+vim.api.nvim_create_user_command("Strip", function()
+  local Job = require "plenary.job"
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local content = table.concat(lines, "\n")
+  local strip = Job:new {
+    command = "git",
+    args = { "stripspace" },
+    writer = content,
+  }
+  strip:sync()
+  local result = strip:result()
+  if #result > 0 then
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, result)
+    if vim.api.nvim_get_option_value('modified', { buf = bufnr }) then
+      vim.cmd('write')
+    end
+  else
+    error("Failed to strip whitespace")
+  end
+end, { desc = "Remove trailing whitespace from current buffer" })
