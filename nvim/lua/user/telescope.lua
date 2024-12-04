548aeba0b7810895598da6a9fb8565b66d65e6ac
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-12-03 20:02:26 -0800

    chore: del unused maps

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>,", builtin.find_files, {})
 vim.keymap.set("n", "<leader>m", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>w", builtin.grep_string, {})
-vim.keymap.set("v", "<leader>w", function()
+vim.keymap.set("v", "<leader>m", function()
   local saved_reg = vim.fn.getreg("v")
   vim.cmd('noau normal! "vy"')
   local selection = vim.fn.getreg("v")
@@ -24,12 +24,6 @@ vim.keymap.set("n", "<leader>/", function()
 end, {})
 vim.keymap.set("n", "<leader>co", builtin.resume, {})
 vim.keymap.set("n", "<leader>d", builtin.git_status, {})
-vim.keymap.set("n", "<leader>n,", function()
-  builtin.find_files { search_dirs = { vim.fn.expand "%:h" } }
-end, {})
-vim.keymap.set("n", "<leader>nm", function()
-  builtin.live_grep { search_dirs = { vim.fn.expand "%:h" } }
-end, {})
 
 require("telescope").setup {
   defaults = {
