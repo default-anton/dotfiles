7a2291bf469b3f3c2a406dc9f3bb500d89a294d2
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-12-11 21:08:52 -0800

    feat: <leader>ya Yank absolute file path

--- nvim/lua/user/maps.lua
+++ nvim/lua/user/maps.lua
@@ -6,6 +6,10 @@ vim.api.nvim_set_keymap('n', '<leader>yf', [[:let @+ = expand('%')<CR>]],
 vim.api.nvim_set_keymap('n', '<leader>yl', [[:let @+ = expand('%') . ':' . line('.')<CR>]],
   { noremap = true, silent = true, desc = "Yank file path with line number" })
 
+-- Yank absolute path of current file
+vim.api.nvim_set_keymap('n', '<leader>ya', [[:let @+ = expand('%:p')<CR>]],
+  { noremap = true, silent = true, desc = "Yank absolute file path" })
+
 -- Quickfix list navigation
 vim.api.nvim_set_keymap('n', '<leader>qo', ':copen<CR>', { noremap = true, silent = true, desc = "Open quickfix list" })
 vim.api.nvim_set_keymap('n', '<leader>qc', ':cclose<CR>', { noremap = true, silent = true, desc = "Close quickfix list" })
