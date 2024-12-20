8a5f3d6b8fb76f1455b5078159fa3f31ac519f95
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-12-19 21:01:20 -0800

    fix: update treesitter context + hide module comments

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -2,7 +2,7 @@ local disabled_langs = { "ruby" }
 
 require("nvim-treesitter.configs").setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "html", "ruby", "lua", "bash", "python", "comment", "dockerfile", "gitattributes", "gitignore", "go", "gomod", "graphql", "hcl", "javascript", "jsdoc", "json", "make", "markdown", "proto", "scss", "sql", "toml", "tsx", "typescript", "vim", "yaml", "dart" },
+  ensure_installed = { "query", "html", "ruby", "lua", "bash", "python", "comment", "dockerfile", "gitattributes", "gitignore", "go", "gomod", "graphql", "hcl", "javascript", "jsdoc", "json", "make", "markdown", "proto", "scss", "sql", "toml", "tsx", "typescript", "vim", "yaml", "dart" },
   highlight = {
     enable = true,
     disable = function(lang, buf)
@@ -66,43 +66,4 @@ require("nvim-treesitter.configs").setup {
     enable = true, -- mandatory, false will disable the whole extension
     disable = {},  -- optional, list of language that will be disabled
   },
-}
-
-require("treesitter-context").setup {
-  enable = true,        -- Enable this plugin (Can be enabled/disabled later via commands)
-  max_lines = 0,        -- How many lines the window should span. Values <= 0 mean no limit.
-  trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  patterns = {          -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-    -- For all filetypes
-    -- Note that setting an entry here replaces all other patterns for this entry.
-    -- By setting the 'default' entry below, you can control which nodes you want to
-    -- appear in the context window.
-    default = {
-      "class",
-      "function",
-      "method",
-      -- 'for', -- These won't appear in the context
-      -- 'while',
-      -- 'if',
-      -- 'switch',
-      -- 'case',
-    },
-    -- Example for a specific filetype.
-    -- If a pattern is missing, *open a PR* so everyone can benefit.
-    --   rust = {
-    --       'impl_item',
-    --   },
-  },
-  exact_patterns = {
-    -- Example for a specific filetype with Lua patterns
-    -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-    -- exactly match "impl_item" only)
-    -- rust = true,
-  },
-
-  -- [!] The options below are exposed but shouldn't require your attention,
-  --     you can safely ignore them.
-
-  zindex = 20,     -- The Z-index of the context window
-  mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
 }
