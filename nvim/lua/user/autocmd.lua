479ee527a2e6a1558bfb1bfbb4c845067da3e7b5
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-12-28 20:19:57 -0800

    feat: Copy terminal output to t register on error

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -4,6 +4,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "nvimrc, .nvimrc",
   command = ":so %",
   group = restart_things,
+  desc = "Reload nvimrc on write",
 })
 
 local yank_group = vim.api.nvim_create_augroup("HighlightYank", { clear = true })
@@ -16,4 +17,28 @@ vim.api.nvim_create_autocmd("TextYankPost", {
       timeout = 150,
     }
   end,
+  desc = "Highlight yanked text for 150ms",
+})
+
+local terminal_group = vim.api.nvim_create_augroup("Terminal", { clear = true })
+
+vim.api.nvim_create_autocmd('TermClose', {
+  group = terminal_group,
+  callback = function(event)
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+    if vim.v.event.status ~= 0 then
+      local output = vim.trim(table.concat(lines, '\n'))
+      local command = event.file:match("^term:[^:]+:(.*)$")
+
+      vim.fn.setreg('t', table.concat({
+        "Command: " .. command,
+        "Output:",
+        "````",
+        output,
+        "````",
+      }, '\n'))
+    end
+  end,
+  desc = "Copy terminal output to t register on error",
 })
