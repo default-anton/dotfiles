98bb89d2a12f563c8ecdc1e8d1abbe1fe8908d44
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2025-01-07 17:09:57 -0800

    chore: del redundant maps

--- nvim/lua/user/gitlinker.lua
+++ nvim/lua/user/gitlinker.lua
@@ -4,20 +4,6 @@ require("gitlinker").setup {
 
 vim.api.nvim_set_keymap(
   "n",
-  "<leader>yr",
-  '<cmd>lua require"gitlinker".get_buf_range_url("n")<cr>',
-  { silent = true, desc = "Copy git link for current line" }
-)
-
-vim.api.nvim_set_keymap(
-  "v",
-  "<leader>yr",
-  '<cmd>lua require"gitlinker".get_buf_range_url("v")<cr>',
-  { desc = "Copy git link for selected lines" }
-)
-
-vim.api.nvim_set_keymap(
-  "n",
   "<leader>gr",
   '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr><cr>',
   { silent = true, desc = "Open git link for current line in browser" }
