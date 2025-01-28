714cc637b34186acafde71f28603b34b433eff49
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2025-01-28 11:46:10 -0800

    feat: update the list of models

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -69,10 +69,11 @@ require("lazy").setup({
     config = function()
       require('llm-sidekick').setup({
         aliases = {
-          deepseek_chat = "deepseek-chat",
+          -- deepseek_chat = "deepseek-chat",
           flash_gemini = "gemini-2.0-flash-exp",
-          llama_70_b_r1 = "groq.deepseek-r1-distill-llama-70b",
-          r1 = "deepseek-reasoner",
+          -- llama_70_b_r1 = "groq.deepseek-r1-distill-llama-70b",
+          -- r1 = "deepseek-reasoner",
+          o1_mini = "o1-mini",
           sonnet = "anthropic.claude-3-5-sonnet-20241022-v2:0",
           think_gemini = "gemini-2.0-flash-thinking-exp-01-21",
         },
