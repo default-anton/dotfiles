2c2fb55118e7dac1b7ccab6d196e9f08c908898d
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2025-02-04 16:08:21 -0800

    feat: gyr to copy git link

--- nvim/lua/user/gitlinker.lua
+++ nvim/lua/user/gitlinker.lua
@@ -4,14 +4,14 @@ require("gitlinker").setup {
 
 vim.api.nvim_set_keymap(
   "n",
-  "<leader>gr",
-  '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr><cr>',
-  { silent = true, desc = "Open git link for current line in browser" }
+  "gyr",
+  '<cmd>lua require"gitlinker".get_buf_range_url("n")<cr>',
+  { silent = true, desc = "Copy git link for current line" }
 )
 
 vim.api.nvim_set_keymap(
   "v",
-  "<leader>gr",
-  '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr><cr>',
-  { desc = "Open git link for selected lines in browser" }
+  "gyr",
+  '<cmd>lua require"gitlinker".get_buf_range_url("v")<cr>',
+  { desc = "Copy git link for selected lines" }
 )
