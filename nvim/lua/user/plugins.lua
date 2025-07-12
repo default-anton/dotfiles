41beea43f06028b7b590bb5cc3624b581b76fb64
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2025-07-11 21:20:36 -0700

    fix: telescope fzf native plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -51,7 +51,7 @@ require("lazy").setup({
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {
     'stevearc/oil.nvim',
     opts = {},
