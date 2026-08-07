aca1b1760df84271e05aadc38d08633c21fd79eb
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2026-08-07 14:24:11 -0700

    nvim: remove cloak plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -63,12 +63,6 @@ require("lazy").setup({
   },
   "nvim-telescope/telescope-ui-select.nvim",
   {
-    'laytan/cloak.nvim',
-    config = function()
-      require('cloak').setup()
-    end
-  },
-  {
     'Wansmer/treesj',
     keys = { 'gm' },
     dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
